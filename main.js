8a950bf8da1a21ef16d4accc90cb1e825f8d7088
Sareddy Keerthana <113521319+Keerthana272002@users.noreply.github.com>
2022-09-24 10:25:18 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,518 @@
+function chatBot() {
+
+    this.input;
+   
+    this.respondTo = function (input) {
+ 
+      this.input = input.toLowerCase();
+ 
+      if (this.match('(hy|hello|hi)'))
+      return "Hi dude";
+ 
+      if (this.match('(how are you|hw r u|how r u )'))
+      return "I'm doing great,thanks for asking!";
+ 
+      if (this.match('(how old are you)'))
+      return "Infinity";
+ 
+      if (this.match('(what is your name)'))
+      return "Robot";
+
+      if(this.match('(61|20251a1261|1261|who is 61)'))
+      return "Sanjana.A from IT(2nd year)";
+
+      if(this.match('(62|20251a1262|1262|who is 62)'))
+      return "Shalini.A from IT(2nd year)";
+
+      if(this.match('(63|20251a1263|1263|who is 63)'))
+      return "Sriya.B from IT(2nd year)";
+
+      if(this.match('(64|20251a1264|1264|who is 64)'))
+      return "Vaishnavi.Ch from IT(2nd year)";
+
+      if(this.match('(65|20251a1265|1265|who is 65)'))
+      return "Sahithi.Ch from IT(2nd year)";
+
+      if(this.match('(66|20251a1266|1266|who is 66)'))
+      return "Aishwarya from IT(2nd year)";
+
+      if(this.match('(67|20251a1267|1267|who is 67)'))
+      return "Padma.D from IT(2nd year)";
+
+      if(this.match('(68|20251a1268|1268|who is 68)'))
+      return "Sanjana.G from IT(2nd year)";
+
+      if(this.match('(69|20251a1269|1269|who is 69)'))
+      return "Harshini from IT(2nd year)";
+
+      if(this.match('(70|20251a1270|1270|who is 70)'))
+      return "Akshitha.K from IT(2nd year)";
+
+      if(this.match('(71|20251a1271|1271|who is 71)'))
+      return "Kalyani.L from IT(2nd year)";
+
+      if(this.match('(72|20251a1272|1272|who is 72)'))
+      return "Poojitha from IT(2nd year)";
+
+      if(this.match('(73|20251a1273|1273|who is 73)'))
+      return "Sowjanya from IT(2nd year)";
+
+      if(this.match('(74|20251a1274|1274|who is 74)'))
+      return "Jyosthna from IT(2nd year)";
+
+      if(this.match('(75|20251a1275|1275|who is 75)'))
+      return "Yashaswini from IT(2nd year)";
+
+      if(this.match('(76|20251a1276|1276|who is 76)'))
+      return "Akshitha from IT(2nd year)";
+
+      if(this.match('(77|20251a1277|1277|who is 77)'))
+      return "Santhoshini from IT(2nd year)";
+
+      if(this.match('(78|20251a1278|1278|who is 78)'))
+      return "Srujana from IT(2nd year)";
+
+      if(this.match('(79|20251a1279|1279|who is 79)'))
+      return "Neha Rudra from IT(2nd year)";
+
+      if(this.match('(80|251a1280|1280|who is 80)'))
+      return "Akshitha from IT(2nd year)";
+
+      if(this.match('(81|20251a1281|1281|who is 81)'))
+      return "Deepthi from IT(2nd year)";
+
+      if(this.match('(82|20251a1282|1282|who is 82)'))
+      return "Sahithi from IT(2nd year)";
+
+      if(this.match('(83|20251a1283|1283|who is 83)'))
+      return "Keerthana.S from IT(2nd year)";
+
+      if(this.match('(84|20251a1284|1284|who is 84)'))
+      return "Shaistha Naaz from IT(2nd year)";
+
+      if(this.match('(85|20251a1285|1285|who is 85)'))
+      return "Shreya Basu from IT(2nd year)";
+
+      if(this.match('(86|20251a1286|1286|who is 86)'))
+      return "Shriya.D from IT(2nd year)";
+
+      if(this.match('(87|20251a1287|1287|who is 87)'))
+      return "Shirisha from IT(2nd year)";
+
+      if(this.match('(88|20251a1288|1288|who is 88)'))
+      return "Siri from IT(2nd year)";
+
+      if(this.match('(89|20251a1289|1289|who is 89)'))
+      return "Keerthi from IT(2nd year)";
+
+       if(this.match('(90|20251a1290|1290|who is 90)'))
+      return "Varshitha from IT(2nd year)";
+
+      if(this.match('(91|20251a1291|1291|who is 91)'))
+      return "Kshetra from IT(2nd year)";
+
+      if(this.match('(92|20251a1292|1292|who is 92)'))
+      return "Yuktha from IT(2nd year)";
+
+      if(this.match('(93|20251a1293|1293|who is 93)'))
+      return "Sravani from IT(2nd year)";
+
+      if(this.match('(94|20251a1294|1294|who is 94)'))
+      return "Rishitha from IT(2nd year)";
+
+      if(this.match('(95|20251a1295|1295|who is 95)'))
+      return "Charmika from IT(2nd year)";
+
+      if(this.match('(96|20251a1296|1296|who is 96)'))
+      return "Ruchitha from IT(2nd year)";
+
+      if(this.match('(97|20251a1297|1297|who is 97)'))
+      return "Nikitha from IT(2nd year)";
+
+      if(this.match('(98|20251a1298|1298|who is 98)'))
+      return "Tejaswi from IT(2nd year)";
+
+       if(this.match('(99|20251a1299|1299|who is 99)'))
+      return "Varshini from IT(2nd year)";
+
+      if(this.match('(a0|20251a12a0|12a0|who is a0)'))
+      return "Varshini from IT(2nd year)";
+
+      if(this.match('(a1|20251a12a1|12a1|who is a1)'))
+      return "Vaishnavi from IT(2nd year)";
+
+      if(this.match('(a2|20251a12a2|12a2|who is a2)'))
+      return "Navya from IT(2nd year)";
+
+      if(this.match('(a3|20251a12a3|12a3|who is a3)'))
+      return "Alekhya from IT(2nd year)";
+
+      if(this.match('(a4|20251a12a4|12a4|who is a4)'))
+      return "Rakshitha from IT(2nd year)";
+
+      if(this.match('(a5|20251a12a5|12a5|who is a5)'))
+      return "Shivani from IT(2nd year)";
+
+      if(this.match('(a6|20251a12a6|12a6|who is a6)'))
+      return "Vishweshwari from IT(2nd year)";
+
+      if(this.match('(a7|20251a12a7|12a7|who is a7)'))
+      return "Kavya from IT(2nd year)";
+
+     if(this.match('(a8|20251a12a8|12a8|who is a8)'))
+      return "Tejaswi from IT(2nd year)";
+
+      if(this.match('(a9|20251a12a9|12a9|who is a9)'))
+      return "Srvya from IT(2nd year)";
+
+      if(this.match('(b0|20251a12b0|12b0|who is b0)'))
+      return "Vasavi from IT(2nd year)";
+
+      if(this.match('(b1|20251a12b1|12b1|who is b1)'))
+      return "Nandhini from IT(2nd year)";
+
+      if(this.match('(b2|20251a12b2|12b2|who is b2)'))
+      return "Christeena from IT(2nd year)";
+
+      if(this.match('(b3|20251a12b3|12b3|who is b3)'))
+      return "Vaishnavi from IT(2nd year)";
+
+      if(this.match('(b4|20251a12b4|12b4|who is b4)'))
+      return "Rishitha from IT(2nd year)";
+
+      if(this.match('(b5|20251a12b5|12b5|who is b5)'))
+      return "Sushma from IT(2nd year)";
+
+      if(this.match('(b6|20251a12b6|12b6|who is b6)'))
+      return "Pallavi from IT(2nd year)";
+
+     if(this.match('(b7|20251a12b7|12b7|who is b7)'))
+      return "Ashritha from IT(2nd year)";
+
+      if(this.match('(b8|20251a12b8|12b8|who is b8)'))
+      return "Sadaf from IT(2nd year)";
+
+      if(this.match('(b9|20251a12b9|12b9|who is b9)'))
+      return "Tejaswi from IT(2nd year)";
+
+      if(this.match('(c0|20251a12c0|12co|who is c0)'))
+      return "Vaishnavi from IT(2nd year)";
+
+      if(this.match('(07|20251a1207|1207|who is 07)'))
+      return "Sowmya from IT(2nd year)";
+
+      if(this.match('(08|20251a1208|1208|who is 08)'))
+      return "Ankitha from IT(2nd year)";
+
+      if(this.match('(09|20251a1209|1209|who is 09)'))
+      return "Sreenidhi from IT(2nd year)";
+
+      if(this.match('(10|20251a1210|1210|who is 10)'))
+      return "saisree from IT(2nd year)";
+
+      if(this.match('(11|20251a1211|1211|who is 11)'))
+      return "Kavya from IT(2nd year)";
+
+      if(this.match('(12|20251a1212|1212|who is 12)'))
+      return "Abhinaya from IT(2nd year)";
+
+      if(this.match('(sanjana a)'))
+      return "20251A1261 from IT(2nd year)";
+
+      if(this.match('(shalini a)'))
+      return "20251a1262 from IT(2nd year)";
+
+      if(this.match('(sriya b)'))
+      return "Sriya.B20251a1263 from IT(2nd year)";
+
+      if(this.match('(vaishnavi ch)'))
+      return "20251a1264 from IT(2nd year)";
+
+      if(this.match('(sahithi ch)'))
+      return "20251a1265 from IT(2nd year)";
+
+      if(this.match('(aishwarya)'))
+      return "20251a1266 from IT(2nd year)";
+
+      if(this.match('(padma d)'))
+      return "20251A1267 from IT(2nd year)";
+
+      if(this.match('(sanjana g)'))
+      return "20251a1268 from IT(2nd year)";
+
+      if(this.match('(harshini k)'))
+      return "20251a1269 from IT(2nd year)";
+
+      if(this.match('(akshitha k)'))
+      return "20251a1270 from IT(2nd year)";
+
+      if(this.match('(kalyani l)'))
+      return "20251a1271 from IT(2nd year)";
+
+      if(this.match('(poojitha)'))
+      return "20251a1272 from IT(2nd year)";
+
+      if(this.match('(sowjanya)'))
+      return "20251a1273 from IT(2nd year)";
+
+      if(this.match('(jyosthna)'))
+      return "20251a1274 from IT(2nd year)";
+
+      if(this.match('(yashaswini)'))
+      return "20251a1275 from IT(2nd year)";
+
+      if(this.match('(akshitha p)'))
+      return "20251a1276 from IT(2nd year)";
+
+      if(this.match('(santhoshini p)'))
+      return "20251a1277 from IT(2nd year)";
+
+      if(this.match('(srujana)'))
+      return "20251a1278 from IT(2nd year)";
+
+      if(this.match('(neha rudra)'))
+      return "20251a1279 from IT(2nd year)";
+
+      if(this.match('(akshitha s)'))
+      return "2051a1280 from IT(2nd year)";
+
+      if(this.match('(deepthi)'))
+      return "20251a1281 from IT(2nd year)";
+
+      if(this.match('(sahithi m)'))
+      return "20251a1282 from IT(2nd year)";
+
+      if(this.match('(keerthana s)'))
+      return "20251a1283 from IT(2nd year)";
+
+      if(this.match('(shaistha naaz)'))
+      return "20251a1284 from IT(2nd year)";
+
+      if(this.match('(sreya basu)'))
+      return "20251a1285 from IT(2nd year)";
+
+      if(this.match('(shriya reddy d)'))
+      return "20251a1286 from IT(2nd year)";
+
+      if(this.match('(shirisha)'))
+      return "20251a1287 from IT(2nd year)";
+
+      if(this.match('(siri)'))
+      return "20251a1288 from IT(2nd year)";
+
+      if(this.match('(keerthi)'))
+      return "20251a1289 from IT(2nd year)";
+
+       if(this.match('(varshitha y)'))
+      return "20251a1290 from IT(2nd year)";
+
+      if(this.match('(kshetra)'))
+      return "20251a1291 from IT(2nd year)";
+
+      if(this.match('(yuktha)'))
+      return "20251a1292 from IT(2nd year)";
+
+      if(this.match('sravani)'))
+      return "20251a1293 from IT(2nd year)";
+
+      if(this.match('(rishitha bandi)'))
+      return "20251a1294 from IT(2nd year)";
+
+      if(this.match('charmika)'))
+      return "20251a1295 from IT(2nd year)";
+
+      if(this.match('(ruchitha)'))
+      return "20251a1296 from IT(2nd year)";
+
+      if(this.match('(nikitha p)'))
+      return "20251a1297 from IT(2nd year)";
+
+      if(this.match('(tejaswini b)'))
+      return "20251a1298 from IT(2nd year)";
+
+       if(this.match('(varshini g)'))
+      return "20251a1299 from IT(2nd year)";
+
+      if(this.match('(varshini b)'))
+      return "20251a12a0 from IT(2nd year)";
+
+      if(this.match('(vaishnavi k)'))
+      return "20251a12a1 from IT(2nd year)";
+
+      if(this.match('(navya)'))
+      return "20251a12a2 from IT(2nd year)";
+
+      if(this.match('(alekhya)'))
+      return "20251a12a3 from IT(2nd year)";
+
+      if(this.match('(rakshitha)'))
+      return "20251a12a4 from IT(2nd year)";
+
+      if(this.match('(shivani)'))
+      return "20251a12a5 from IT(2nd year)";
+
+      if(this.match('(vishweshwari)'))
+      return "20251a12a6 from IT(2nd year)";
+
+      if(this.match('(kavya s)'))
+      return "20251a12a7 from IT(2nd year)";
+
+     if(this.match('(tejawsini r)'))
+      return "20251a12a8 from IT(2nd year)";
+
+      if(this.match('(sravya)'))
+      return "20251a12a9 from IT(2nd year)";
+
+      if(this.match('(vasavi)'))
+      return "20251a12b0 from IT(2nd year)";
+
+      if(this.match('(nandhini)'))
+      return "20251a12b1 from IT(2nd year)";
+
+      if(this.match('(christeena)'))
+      return "20251a12b2 from IT(2nd year)";
+
+      if(this.match('(vaishnavi m)'))
+      return "20251a12b3 from IT(2nd year)";
+
+      if(this.match('(rishitha)'))
+      return "20251a12b4 from IT(2nd year)";
+
+      if(this.match('(sushma)'))
+      return "20251a12b5 from IT(2nd year)";
+
+      if(this.match('(pallavi)'))
+      return "20251a12b6 from IT(2nd year)";
+
+     if(this.match('(ashritha)'))
+      return "20251a12b7 from IT(2nd year)";
+
+      if(this.match('(sadaf)'))
+      return "20251a12b8 from IT(2nd year)";
+
+      if(this.match('(tejaswi)'))
+      return "20251a12b9 from IT(2nd year)";
+
+      if(this.match('(vaishnavi ch)'))
+      return "20251a12c0 from IT(2nd year)";
+
+      if(this.match('(sowmya)'))
+      return "20251a1207 from IT(2nd year)";
+
+      if(this.match('(ankitha)'))
+      return "20251a1208 from IT(2nd year)";
+
+      if(this.match('(sreenidhi)'))
+      return "20251a1209 from IT(2nd year)";
+
+      if(this.match('(saisree)'))
+      return "20251a1210 from IT(2nd year)";
+
+      if(this.match('(kavya)'))
+      return "20251a1211 from IT(2nd year)";
+
+      if(this.match('(abhinaya)'))
+      return "20251a1212 from IT(2nd year)";
+
+      if(this.match('(hod of it|who is the hod of it)'))
+      return "Ravi Prakash"
+
+      if(this.match('(principal of gnits|who is the principal of gnits|gnits principal)'))
+      return "Ramesh Reddy"
+
+      if(this.match('(average pass percentage|minimum pass percentage)'))
+      return "85%"
+
+      if(this.match('(highest package)'))
+      return "47 lakhs"
+
+      if(this.match('(average package|minimum package offered for the student)'))
+      return "3.5lakhs"
+
+      if(this.match('(i love you|i luv u|143)'))
+      return"I love you too"
+
+
+      return input + ", I don't understand what it is";
+    };
+ 
+   
+    this.match = function (regex) {
+ 
+      return new RegExp(regex).test(this.input);
+    };
+  }
+ 
+ 
+  $(function () {
+ 
+   
+    var you = 'You';
+    var robot = 'Chatbot';
+ 
+   
+    var delayStart = 400;
+    var delayEnd = 800;
+ 
+   
+    var bot = new chatBot();
+    var chat = $('.chat');
+    var waiting = 0;
+    $('.busy').text(robot + ' is typing...');
+ 
+   
+    var submitChat = function () {
+ 
+      var input = $('.input input').val();
+      if (input == '') return;
+ 
+      $('.input input').val('');
+      updateChat(you, input);
+ 
+      var reply = bot.respondTo(input);
+      if (reply == null) return;
+ 
+      var latency = Math.floor(Math.random() * (delayEnd - delayStart) + delayStart);
+      $('.busy').css('display', 'block');
+      waiting++;
+      setTimeout(function () {
+        if (typeof reply === 'string') {
+          updateChat(robot, reply);
+        } else {
+          for (var r in reply) {
+            updateChat(robot, reply[r]);
+          }
+        }
+        if (--waiting == 0) $('.busy').css('display', 'none');
+      }, latency);
+    };
+ 
+    var updateChat = function (party, text) {
+ 
+      var style = 'you';
+      if (party != you) {
+        style = 'other';
+      }
+ 
+      var line = $('<div><span class="party"></span> <span class="text"></span></div>');
+      line.find('.party').addClass(style).text(party + ':');
+      line.find('.text').text(text);
+ 
+      chat.append(line);
+ 
+      chat.stop().animate({ scrollTop: chat.prop("scrollHeight") });
+ 
+    };
+ 
+ 
+    $('.input').bind('keydown', function (e) {
+      if (e.keyCode == 13) {
+        submitChat();
+      }
+    });
+    $('.input a').bind('click', submitChat);
+ 
+ 
+    updateChat(robot, 'Hello write me something');
+ 
+  });
